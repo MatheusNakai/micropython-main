e43da0b8bb286d5338e684b73eb2ef701e7911c3
MatheusNakai <79463186+MatheusNakai@users.noreply.github.com>
2022-04-28 08:21:26 -0300

    Update Oled_controller.py

--- controllers/Oled_controller.py
+++ controllers/Oled_controller.py
@@ -8,3 +8,13 @@ class Oled_controller(object):
 
     def Oled_display(self, text:str, x:int, y:int):
         self.oled.Oled_display(text, x, y)
+
+    def Oled_clear(self):
+        self.oled.Oled_clear()
+    
+    def Oled_on(self):
+        self.oled.Oled_on()
+    
+    def Oled_off(self):
+        self.oled.Oled_off()
+        
