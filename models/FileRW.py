4646ae74d96e98b1ba968269249fdf9e99b33fcb
MatheusNakai <79463186+MatheusNakai@users.noreply.github.com>
2022-04-28 17:58:01 -0300

    Update FileRW.py

--- models/FileRW.py
+++ models/FileRW.py
@@ -1,15 +1,16 @@
-import json
+import ujson
 
 class FileRW(object):
     
     def __init__(self, file_name:str):
         self.file_name = file_name
+        
     
     def write(list_dict):
         try:
             with open('json.txt','a') as f:
                 for user in list_dict:
-                    json = json.dump(user)
+                    json = ujson.dump(user)
                     f.write(f'{json}\n')
         except:
             print("Error writing to file")
