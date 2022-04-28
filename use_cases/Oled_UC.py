96d632b7161f1f34901739b3701cbaa044883da1
MatheusNakai <79463186+MatheusNakai@users.noreply.github.com>
2022-04-28 08:21:57 -0300

    Update Oled_UC.py

--- use_cases/Oled_UC.py
+++ use_cases/Oled_UC.py
@@ -7,3 +7,12 @@ class Oled_UC(object):
         
     def display_text(self,text,x,y):
         self.oled_controller.display_text(text,x,y)
+
+    def display_clear(self):
+        self.oled_controller.display_clear()
+
+    def display_on(self):
+        self.oled_controller.display_on()
+    
+    def display_off(self):
+        self.oled_controller.display_off()
