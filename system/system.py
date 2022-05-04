6d9d9e854acc0a961f130364502eea7d328343aa
MatheusNakai <79463186+MatheusNakai@users.noreply.github.com>
2022-05-04 18:12:25 -0300

    Update system.py

--- system/system.py
+++ system/system.py
@@ -1,6 +1,7 @@
 from use_cases.FileRW_UC import FileRW_UC
 from use_cases.Keypad_UC import Keypad_UC
 from use_cases.Oled_UC import Oled_UC
+from use_cases.Servo_UC import Servo_UC
 from models.User import User
 from machine import Pin, I2C
 import utime
@@ -10,7 +11,11 @@ def check_password(list_of_dict, password):
     Check if the password is correct.
     """
     for user in list_of_dict:
+<<<<<<< Updated upstream
         if user["password"] == password:
+=======
+        if password==user["password"]:
+>>>>>>> Stashed changes
             return True
     return False
 
@@ -24,17 +29,19 @@ def run():
     led.low()
     oled = Oled_UC()
     keypad = Keypad_UC()
-    fileRW= FileRW_UC()
+    fileRW= FileRW_UC('json.txt')
+    servo = Servo_UC()
 
     list_user = fileRW.read()
+    print(list_user)
     non_number = ["A","B","C","D","*","#"]
     number = ["1","2","3","4","5","6","7","8","9","0"]
     password = ""
     i=0
-    oled.Oled_text("Welcome",0,0)
+    oled.display_text("Welcome",0,0)
 
     while 200>i:
-        if movement_sensor.value()==0:
+        if movement_sensor.value()==1:
             led.low()
             utime.sleep(0.2)
             oled.display_clear()
@@ -46,12 +53,13 @@ def run():
 
             oled.display_clear()
             oled.display_on()
-            oled.Oled_text("Enter Password:",0,0)
+            oled.display_text("Enter Password:",0,0)
             key = keypad.Keypad4x4Read()
             if key in non_number:
                 if key == non_number[0]:
                     # Turn off the display, clear password and lock door
                     oled.display_off()
+                    servo.close_door()
                     try:
                         password = ""
                     except:
@@ -72,6 +80,7 @@ def run():
                     oled.display_clear()
                     # Submit the password
                     if len(password)<5:
+<<<<<<< Updated upstream
                         oled.Oled_text("Password too short",0,1)
 
             if key in number:
@@ -81,10 +90,35 @@ def run():
             if len(password)==5:
                     if check_password(list_user, password):
                         oled.Oled_text("Welcome",0,0)
+=======
+                        oled.display_text("Password too short",0,1)
+
+            if key in number:
+                password += key
+                oled.display_clear()
+                print(type(key))
+                oled.display_text(password,0,1)
+
+            if len(password)==4:
+                    if check_password(list_user, password):
+                        oled.display_text("Welcome",0,0)
+>>>>>>> Stashed changes
                         oled.display_on()
                         utime.sleep(1)
                         oled.display_clear()
                         oled.display_off()
                         # Start cervo motor to unlock door
+<<<<<<< Updated upstream
 
                     
+=======
+                        servo.open_door()
+                    else:
+                        oled.display_clear()
+                        oled.display_text('Password denied',0,0)
+                        utime.sleep(1)
+                        servo.close_door()
+                    password=''
+
+                    
+>>>>>>> Stashed changes
